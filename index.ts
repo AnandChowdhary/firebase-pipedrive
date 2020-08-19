8c9aa64fe1899efd9f543c6b29e06689eee02d3d
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-19 17:00:20 +0530

    :heavy_plus_sign: Add dotenv

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
 import axios from "axios";
+import { config } from "dotenv";
 const API_KEY = process.env.PIPEDRIVE_API_TOKEN;
+const BASE_URL = "https://koj.pipedrive.com/api/v1";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getPersons = async () => {
+  try {
+    const { data } = await api.get(`/persons?api_token=${API_KEY}`);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getPersons();

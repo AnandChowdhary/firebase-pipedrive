90a02c0f4bfc9c454e2268afe63ab55ca9553e59
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-20 09:57:25 +0530

    :sparkles: Create a lead in Pipedrive

--- index.ts
+++ index.ts
@@ -1,19 +1,56 @@
 import axios from "axios";
 import { config } from "dotenv";
-const API_KEY = process.env.PIPEDRIVE_API_TOKEN;
+config();
+
+const API_KEY = process.env.API_KEY;
 const BASE_URL = "https://koj.pipedrive.com/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getPersons = async () => {
-  try {
-    const { data } = await api.get(`/persons?api_token=${API_KEY}`);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
+interface Person {
+  name: string;
+  email: string[];
+  phone?: string[];
+}
+
+interface Lead {
+  title: string;
+  note?: string;
+  person_id?: number;
+  value?: {
+    amount: number;
+    currency: string;
+  };
+  expected_close_date?: string;
+}
+
+export const addPerson = async (
+  person: Person
+): Promise<{
+  data: {
+    id: number;
+  };
+}> => {
+  const { data } = await api.post(`/persons?api_token=${API_KEY}`, person);
+  console.log("Added user", person.name);
+  return data;
 };
 
-getPersons();
+export const addLead = async (lead: Lead) => {
+  const { data } = await api.post(`/leads?api_token=${API_KEY}`, lead);
+  console.log("Added lead", lead.title);
+  return data;
+};
+
+(async () => {
+  const person = await addPerson({
+    name: "Anand Chowdhary",
+    email: ["anand@koj.co"],
+  });
+  await addLead({
+    person_id: person.data.id,
+    title: "Anand Chowdhary's Bern apartment",
+  });
+})();
